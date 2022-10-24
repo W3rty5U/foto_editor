932bc3b3357ff81a0f509474cd2895ba673fc97f
Filip Majchrzak <filip.majchrzak@erecept.pl>
2022-10-24 17:00:55 +0200

    rotate image based on EXIF data

--- app/src/main/java/com/example/fotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/fotoeditor/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.fotoeditor
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +22,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var takePictureButton: Button
 
+    fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(
+            source, 0, 0, source.width, source.height,
+            matrix, true
+        )
+    }
+
     private var contract = registerForActivityResult(ActivityResultContracts.TakePicture()) {
         if (it) {
             val imgFile = File(cacheDir, PHOTO_FILENAME)
             if (imgFile.exists()) {
-                val bmp = BitmapFactory.decodeFile(imgFile.absolutePath)
+                val ei = ExifInterface(imgFile.absolutePath)
+                val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+                var bmp = BitmapFactory.decodeFile(imgFile.absolutePath)
+                bmp = when (orientation) {
+                    ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bmp, 90f)
+                    ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bmp, 180f)
+                    ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bmp, 270f)
+                    else -> { bmp }
+                }
                 imageView.setImageBitmap(bmp)
             }
         }
